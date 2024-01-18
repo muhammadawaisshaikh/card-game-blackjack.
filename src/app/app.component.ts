0c2a784383db7ecae1a56a08a17366941b4bcfb9
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-18 22:43:24 +0500

    consumed services in card game in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BlackjackService } from './services/black-jeck/black-jeck.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,32 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'card-game-blackjack';
+  playerHand: any = [];
+  dealerHand: any = [];
+
+  constructor(private blackjackService: BlackjackService) { }
+
+  startGame(): void {
+    this.blackjackService.startGame();
+    this.playerHand = this.blackjackService.getPlayerHand();
+    this.dealerHand = this.blackjackService.getDealerHand();
+  }
+
+  hit(): void {
+    this.blackjackService.hit();
+    this.playerHand = this.blackjackService.getPlayerHand();
+  }
+
+  stand(): void {
+    this.blackjackService.stand();
+    this.dealerHand = this.blackjackService.getDealerHand();
+  }
+
+  isGameStarted(): boolean {
+    return this.blackjackService.onGameStarted();
+  }
+
+  isGameOver(): boolean {
+    return this.blackjackService.onGameOver();
+  }
 }
